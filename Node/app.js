09ac5b49725f919c1c0cf0ec931cc8f97dab95b5
Bruno Marini <bruno.marini1@gmail.com>
2018-06-04 14:51:46 -0300

    end -> send

--- Node/app.js
+++ Node/app.js
@@ -1,7 +1,7 @@
 var app =  require('express')();
 
 app.get('/produtos', function(request, response){
-    response.end("<html><body><h1>Listagem de produtos</h1></body></html>");
+    response.send("<html><body><h1>Listagem de produtos</h1></body></html>");
 });
 
 app.listen(80, function(){
