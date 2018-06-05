88b21fc06074393a250ebd521d7359a0ad6fc8e5
Bruno Marini <bruno.marini1@gmail.com>
2018-06-05 15:34:27 -0300

    Database Connection

--- Node/app/routes/produtos.js
+++ Node/app/routes/produtos.js
@@ -1,12 +1,7 @@
+var connection = require('../infra/dbConnection')();
+
 module.exports = function(app) {
     app.get('/produtos', function(request, response) {
-        var mysql = require('mysql');
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'root',
-            database: 'casadocodigo_nodejs'
-        });
 
         connection.query('select * from livros', function(err, results) {
             //response.send(results);
