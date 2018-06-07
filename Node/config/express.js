e86d0474f5f9e3410589bfb6a2c8b0960531e3ab
Bruno Marini <bruno.marini1@gmail.com>
2018-06-07 00:32:39 -0300

    body-parser (require & use)

--- Node/config/express.js
+++ Node/config/express.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser');
 
 module.exports = () => {
   var app = express();
 
-  //app.use(express.static('./app/public'));
+  app.use(bodyParser.urlencoded({extended: true}));
+
+  //requisição -> middlewareBodyParsers -> middlerwareAutenticação -> função que trata a requisição
 
   app.set('view engine', 'ejs');
   app.set('views', './app/views');
