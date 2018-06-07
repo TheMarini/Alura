ed6cb38bba5b55f522751a6f8e4e334cae5f4692
Bruno Marini <bruno.marini1@gmail.com>
2018-06-07 00:30:30 -0300

    Arrow Function

--- Node/app/infra/ProductsDAO.js
+++ Node/app/infra/ProductsDAO.js
@@ -1,13 +1,15 @@
-class ProductsDAO {
-  constructor(connection) {
-    this._connection = connection;
+module.exports = () =>
+  class ProductsDAO {
+    constructor(connection) {
+      this._connection = connection;
+    }
+    lista(callback) {
+      this._connection.query('select * from livros', callback);
+    }
+    salva(produto, callback) {
+      this._connection.query('insert into livros set ?', produto, callback);
+    }
   }
-  lista(callback){
-    this._connection.query('select * from livros', callback);
-  }
-}
-
-module.exports = () => ProductsDAO;
 
 ///////////////////////
 
