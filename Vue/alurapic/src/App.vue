b541d14d07e87965d8e2d9b1b11292999670ea32
Bruno Marini <bruno.marini1@gmail.com>
2018-07-02 17:40:06 -0300

    Fotos | v-for

--- Vue/alurapic/src/App.vue
+++ Vue/alurapic/src/App.vue
@@ -1,60 +1,34 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+<div id="app">
+  <h1>{{ titulo }}</h1>
+  <div class="items">
+    <div class="item" v-for="foto of fotos">
+      <img :src="foto.url" :alt="foto.alt">
+    </div>
   </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      titulo: 'Alurapic',
+      fotos: [
+        {
+          url: 'http://tudosobrecachorros.com.br/wp-content/uploads/cachorro-independente.jpg',
+          alt: 'Cachorro'
+        },
+        {
+          url: 'https://t1.uc.ltmcdn.com/pt/images/8/7/2/img_como_evitar_a_queda_de_pelo_de_cachorro_6278_600.jpg',
+          alt: 'Cachorro'
+        }
+      ]
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
