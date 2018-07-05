b88a45b6637c043f298eb870239fcb9d11cc379a
Bruno Marini <bruno.marini1@gmail.com>
2018-07-05 10:34:36 -0300

    Photos from http get

--- Vue/alurapic/src/App.vue
+++ Vue/alurapic/src/App.vue
@@ -14,17 +14,15 @@ export default {
   data() {
     return {
       titulo: 'Alurapic',
-      fotos: [
-        {
-          url: 'http://tudosobrecachorros.com.br/wp-content/uploads/cachorro-independente.jpg',
-          alt: 'Cachorro'
-        },
-        {
-          url: 'https://t1.uc.ltmcdn.com/pt/images/8/7/2/img_como_evitar_a_queda_de_pelo_de_cachorro_6278_600.jpg',
-          alt: 'Cachorro'
-        }
-      ]
+      fotos: []
     }
+  },
+
+  created() {
+    this.$http.get('http://localhost:3000/v1/fotos') //Return a Promise
+      .then(res => console.log(res.body))
+    //.then(tes => res.json()) -> return a Promise
+    //.then(fotos => console.log(fotos));
   }
 }
 </script>
